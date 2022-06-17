c7537758b4ecb9b0139829344d9b4e3c0a955dba
TuanND0510 <tuanabcxyz555@gmail.com>
2022-06-17 08:24:20 +0700

    cpp

--- subproject/self/main.cpp
+++ subproject/self/main.cpp
@@ -1,17 +1,37 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
 
 int main(){
-    cv::Mat image;
-    image = cv::imread("test.jpg",cv::IMREAD_COLOR);
-    if(! image.data){
-        std::cout<<"Could not open file" << std::endl;
+    Mat input, intermediate , output;
+    input = imread("test.jpg", IMREAD_COLOR);
+    if(! input.data){
+        cout<<"Could not open file" << endl;
         return -1;
     } 
     
-    cv::namedWindow("namba image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("namba image", image);
-    cv::waitKey(0);
+    namedWindow("input image", WINDOW_AUTOSIZE);
+    imshow("input image", input);
+    waitKey(0);
+
+    resize(input, intermediate, Size(620,480)); //resize 620,480
+    cvtColor(intermediate, intermediate, COLOR_BGR2GRAY); // gray
+    bilateralFilter(intermediate, intermediate, 11, 17, 17, BORDER_DEFAULT); // blur
+    // Canny(intermediate, intermediate, 30,200); // perform edge detection
+
+    // find contour
+    // findContours(intermediate, intermediate, RETR_TREE, CHAIN_APPROX_SIMPLE);
+
+
+
+    
+
+    imshow("intermediate",intermediate);
+    waitKey(0);
+    destroyAllWindows();
     
     return 0;
 }
